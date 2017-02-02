7ea09bd9216e74a1d3518713ea80a73f4a8e477a
Joonatan Samuel <Joonatan.samuel@gmail.com>
2017-02-02 03:05:23 +0200

    parse_data(X, Y, ...) is now ready to make us some data for training

--- data_music.py
+++ data_music.py
@@ -3,52 +3,49 @@ import time
 import sounddevice as sd
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+import sys
 
 train_data = np.load(open('musicnet.npz', 'rb'))
 
 print len(train_data.files)
 
 # load samples from song 1788 to X and Y
-X, Y = train_data['1788']
-
-fs = 44100
-fig = plt.figure(figsize=(20,5))
-plt.plot(X[0: 30*fs], color=(41/255, 104/255, 168/255))
-fig.axes[0].set_xlim([0, 30*fs])
-
-# Play audio
-sd.play(X[0: 30 * fs])
-
-# Parse the labels into Yvec
-stride = 512                         # 512 samples between windows
-wps = fs/float(stride)               # ~86 windows/second
-Yvec = np.zeros((int(30*wps),128))   # 128 distinct note labels
-colors = {41 : .33, 42 : .66, 43 : 1}
-
-for window in range(Yvec.shape[0]):
-        labels = Y[window*stride]
-        for label in labels:
-            Yvec[window,label.data[1]] = colors[label.data[0]]
+X, Y = train_data[random.choice(train_data.keys())]
 
 
-fig = plt.figure(figsize=(20,5))
-plt.imshow(Yvec.T,aspect='auto',cmap='ocean_r')
-plt.gca().invert_yaxis()
-fig.axes[0].set_xlabel('window')
-fig.axes[0].set_ylabel('note (MIDI code)')
+def parse_data(X, Y, stride=2048, window_size=512, n_frequency=200, plot=False):
+    # samples per second
+    fs = 44100
+
+    # Parse the labels into Yvec
+    stride = 2048                         # samples between windows
+    Yvec = np.zeros((int(X.shape[0] / stride), 128))   # 128 distinct note labels
+
+    # from intervaltree to "onehot" matrix
+    for window in range(Yvec.shape[0]):
+            labels = Y[window*stride] 
+            for label in labels:
+                    Yvec[window, label.data[1]] = 1
 
 
-# Spectograms of the data
+    # Spectograms of the data
+    # Initialize the empty Xs
+    Xs = np.empty([X.shape[0] / stride - window_size, n_frequency]) # max size: stride * (n_chunks + window_size) = samples; solve for n_chunks
 
-window_size = 2048
-Xs = np.empty([int(30 * wps), 2048])
+    for i in range(Xs.shape[0]):
+        # Do fft computation and then take the energy of that frequency
+        fft_temp = fft(X[i * stride : i * stride + window_size])
+        Xs[i] = np.abs(fft_temp[:n_frequency])
 
-for i in range(Xs.shape[0]):
-    Xs[i] = np.abs(fft(X[i * stride : i * stride + window_size]))
+    if plot:
+        plt.imshow(Xs.T, cmap="Greys", interpolation="nearest")
+        plt.show()
+        plt.imshow(Yvec[:Xs.shape[0], :].T, cmap="Greys", interpolation="nearest")
+        plt.show()
 
-fig = plt.figure(figsize=(20,7))
-plt.imshow(Xs.T[0:150],aspect='auto')
-plt.gca().invert_yaxis()
-fig.axes[0].set_xlabel('windows (~86Hz)')
-fig.axes[0].set_ylabel('frequency')
-plt.show()
+    return Xs, Yvec[:Xs.shape[0], :]
+
+
+xf, y = parse_data(X, Y, plot=True)
+print xf.shape, y.shape
