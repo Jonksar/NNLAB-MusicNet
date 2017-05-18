43737420bc168ebe218e7c9654aeab28159d255d
Joonatan Samuel <Joonatan.Samuel@starship.xyz>
2017-05-18 15:31:25 +0300

    changes to directory structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# NNLAB-MusicNet
-Small project producing note classification on sound data. Uses MusicNet as the dataset.
--- data.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import h5py 
-
-f = h5py.File("cache.hdh5", 'w')
-
-dataset = f.create_dataset('cacheName', (n_data, n_channels, ))
-https://www.kaggle.com/c/state-farm-distracted-driver-detection/forums/t/20664/data-can-t-fit-in-memory
